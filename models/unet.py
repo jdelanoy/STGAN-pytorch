f6932a5fe386a661a39c7c41046424230fa46dc0
Johanna Delanoy <johanna@pop-os.localdomain>
2020-12-12 17:03:34 +0100

    option to use IGN gradients or not

--- models/unet.py
+++ models/unet.py
@@ -149,10 +149,11 @@ class IntrinsicsSplit(autograd_fn.Function):
 
 
 class UNet(nn.Module):
-    def __init__(self, in_ch, out_ch, ch, act, norm):
+    def __init__(self, in_ch, out_ch, ch, act, norm, ign_grad=False):
         super(UNet, self).__init__()
         self.up_and_concat = UpAndConcat()
-
+        self.ign_grad=ign_grad
+        
         # TODO: iterate over ch and automatically create the encoder and decoder
         assert len(ch) == 5
 
@@ -194,7 +195,8 @@ class UNet(nn.Module):
         for block in self.bottleneck:
             x = block(x)
 
-        x = self.split_bneck(x, mode)
+        if self.ign_grad:
+            x = self.split_bneck(x, mode)
 
         return x, x_encoder
 
@@ -222,8 +224,8 @@ class UNet(nn.Module):
 
 
 class Autoencoder(UNet):
-    def __init__(self, in_ch, out_ch, ch, act, norm):
-        super(Autoencoder, self).__init__(in_ch, out_ch, ch, act, norm)
+    def __init__(self, in_ch, out_ch, ch, act, norm, ign_grad=False):
+        super(Autoencoder, self).__init__(in_ch, out_ch, ch, act, norm, ign_grad)
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
         self.up_and_concat = UpAndConcat()
 
