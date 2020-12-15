084a7c7dc8e39ea9fd874ee1151e1ac067109600
Johanna Delanoy <johanna.delanoy@unizar.es>
2020-12-15 12:43:30 +0100

    add disentangle test

--- agents/stgan.py
+++ agents/stgan.py
@@ -215,7 +215,36 @@ class STGANAgent(object):
         save_image(self.denorm(x_concat.data.cpu()),path,
                     nrow=1, padding=0)
 
+    def compute_disentangle_grid(x_real, a_att, path,writer=False):
+        #encode all the batch
+        encodings,bneck = self.G.encode(x_real)
+        splitted_bneck = self.split_bneck(bneck)
+        bneck_size = bneck.shape
 
+        #for each branch
+        for label in range(len(splitted_bneck)):
+            x_fake_list = [torch.cat((x_real[0].unsqueeze(0),x_real),dim=0)]
+                #each column: all share the same embedding for the branchfor c in range(batch_size):
+                bneck_copy = [enc.clone() for enc in splitted_bneck] #[[enc.clone() for enc in encs] for encs in encodings]
+                common_features=bneck_copy[label][c]
+                #change encoding for all images
+                for i in range(batch_size): #for each image
+                    bneck_copy[label][i]=common_features
+                # for layer in range(len(common_features)): #for each layer
+                #     for i in range(img.shape[0]): #for each image
+                #         bneck_copy[label][layer][i]=common_features[layer][c]
+                #decode for the encodings in bneck_copy
+                bneck = self.join_bneck(bneck_copy, bneck_size)
+                fake_image=self.G.decode(bneck,a_att,encodings)
+                #add reference image
+                fake_image=torch.cat((img[c].unsqueeze(0),fake_image),dim=0)
+                x_fake_list.append(fake_image)
+            x_concat = torch.cat(x_fake_list, dim=3)
+            save_image(self.denorm(x_concat.data.cpu()),path.format(label),
+                        nrow=1, padding=0)
+            if writer:
+                self.writer.add_image('disentangle_{}'.format(label), make_grid(self.denorm(x_concat.data.cpu()), nrow=1),
+                                        self.current_iteration)
 
     ################################################################
     ##################### MAIN FUNCTIONS ###########################
@@ -238,7 +267,24 @@ class STGANAgent(object):
         finally:
             self.finalize()
 
+    def split_bneck(self, bneck, do_norm=False):
+        self.bneck_shape = bneck.shape
+        batch_size = bneck.size(0)
+        step = bneck.size(1) // 3
 
+        bneck_material = bneck[:, :step].view(batch_size, -1)  # 1/3 of the features
+        bneck_shape = bneck[:, step:step * 2].view(batch_size, -1)
+        bneck_illum = bneck[:, step * 2:].view(batch_size, -1)
+
+        if do_norm:
+            bneck_material = bneck_material / bneck_material.norm(p=2)
+            bneck_shape = bneck_shape / bneck_shape.norm(p=2)
+            bneck_illum = bneck_illum / bneck_illum.norm(p=2)
+
+        return bneck_material, bneck_shape, bneck_illum
+
+    def join_bneck(self, *bnecks):
+        return torch.cat(bnecks, dim=1).view(*self.bneck_shape)
 
     def train(self):
         self.setup_all_optimizers()
@@ -429,9 +475,8 @@ class STGANAgent(object):
                 if (self.current_iteration) % self.config.sample_step == 0:
                     self.G.eval()
                     with torch.no_grad():
-                        self.compute_sample_grid(Ia_sample,b_samples,a_sample,os.path.join(self.config.sample_dir, 'sample_{}.jpg'.format(self.current_iteration))
-                                                ,writer=True)
-
+                        self.compute_sample_grid(Ia_sample,b_samples,a_sample,os.path.join(self.config.sample_dir, 'sample_{}.jpg'.format(self.current_iteration)),writer=True)
+                        self.compute_disentangle_grid(Ia_sample,a_sample,os.path.join(self.config.sample_dir, 'disentangle_{}_{}.jpg'.format(self.current_iteration,"{}")),writer=True)
                 # save checkpoint
                 if self.current_iteration % self.config.checkpoint_step == 0:
                     self.save_checkpoint()
@@ -509,6 +554,9 @@ class STGANAgent(object):
                 c_trg_list.insert(0, c_org)
                 self.compute_sample_grid(x_real,c_trg_list,c_org,os.path.join(self.config.result_dir, 'sample_big_{}_{}.jpg'.format(i + 1,self.config.checkpoint)),writer=False)
 
+
+
+
     def test_disentangle(self):
         self.load_checkpoint()
         self.G.to(self.device)
@@ -517,37 +565,13 @@ class STGANAgent(object):
                           desc='Testing at checkpoint {}'.format(self.config.checkpoint))
 
         self.G.eval()
-        [C.eval() for C in self.Cs]
         with torch.no_grad():
-            for batch, (x_real, a_att, labels) in enumerate(tqdm_loader): #MANU change to your outputs of the loader
+            for batch, (x_real, a_att, _) in enumerate(tqdm_loader): #MANU change to your outputs of the loader
                 x_real=x_real.to(self.device)
                 a_att=a_att.to(self.device)
-                #encode all the batch
-                #MANU encodings should be the list of encodings of the 3 branches
-                #MANU encodings[i] are all the feature maps of one branche, not only the latent vector (for skip connections)
-                encodings = [C(x_real)[0] for C in self.Cs] #through the attribute branches
-                z = self.G.encode(x_real) #through main encoder
-                encodings.append(z)
-                #for each branch
-                for label in range(len(encodings)):
-                    x_fake_list = [torch.cat((x_real[0].unsqueeze(0),x_real),dim=0)]
-                    #each column: all share the same embedding for the branch
-                    for c in range(x_real.shape[0]):
-                        encodings_copy = [[enc.clone().to(self.device) for enc in encs] for encs in encodings]
-                        common_features=encodings_copy[label]
-                        #change encoding for all images
-                        for layer in range(len(common_features)): #for each layer
-                            for i in range(x_real.shape[0]): #for each image
-                                encodings_copy[label][layer][i]=common_features[layer][c]
-                        #MANU decode for the encodings in encodings_copy, change to your net
-                        fake_image=self.G.decode_from_disentangled(encodings_copy[-1][-1],a_att- a_att if self.config.use_attr_diff else a_att,encodings_copy[:-1])
-                        #add column to list of images
-                        fake_image=torch.cat((x_real[c].unsqueeze(0),fake_image),dim=0)
-                        x_fake_list.append(fake_image)
-                    x_concat = torch.cat(x_fake_list, dim=3)
-                    path=os.path.join(self.config.result_dir, 'sample_disentangle_{}_{}_{}.jpg'.format(batch + 1,label,self.config.checkpoint))
-                    save_image(self.denorm(x_concat.data.cpu()),path,
-                                nrow=1, padding=0)
+                path=os.path.join(self.config.result_dir, 'sample_disentangle_{}_{}_{}.jpg'.format(batch + 1,"{}",self.config.checkpoint))
+                compute_disentangle_grid(x_real,a_att,path)
+                
 
   
 
